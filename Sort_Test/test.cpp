f8202141ad579c51924b17fa7d280c63de01ec0d
Андрей Литвинов <andreoyy.litvinovv@outlook.com>
2022-07-09 15:07:48 +0300

    ukr commentaries added

--- Sort_Test/test.cpp
+++ Sort_Test/test.cpp
@@ -1,7 +1,7 @@
 ﻿#include "pch.h"
 #include "..\Summer_Practice\sort_arr.c"
 
-//Function that compares two array on identity
+//Функція яка перевіряє два масива на ідентичність
 int comp(int arr1[], int arr2[])
 {
 	for (int i = 0; i < size; i++)
@@ -13,15 +13,15 @@ int comp(int arr1[], int arr2[])
 
 TEST(getMax_test, performance)
 {
-	//Test 1
+	//Тест 1
 	int test1[] = { -132, -25, 345, 99, 123, 2004, 9, 0, 3, 10 };
 	int max1 = 2004;
 
-	//Test 2
+	//Тест 2
 	int test2[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int max2 = 9573;
 
-	//Test 3
+	//Тест 3
 	int test3[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int max3 = 9804;
 
@@ -31,19 +31,19 @@ TEST(getMax_test, performance)
 }
 TEST(mergeSort_test, performance)
 {
-	//Test 1
+	//Тест 1
 	int test1[] = { -132, -25, 345, 99, 123, 2004, 9, 0, 3, 10 };
 	int test1_sorted[] = { -132, -25, 0, 3, 9, 10, 99, 123, 345, 2004 };
 
-	//Test 2
+	//Тест 2
 	int test2[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test2_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
 
-	//Test 3
+	//Тест 3
 	int test3[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int test3_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804 };
 
-	//Sorting test arrays
+	//Сортуємо масиви
 	mergeSort(test1, 0, size - 1);
 	mergeSort(test2, 0, size - 1);
 	mergeSort(test3, 0, size - 1);
@@ -56,15 +56,15 @@ TEST(mergeSort_test, performance)
 
 TEST(countingSort_test, performance)
 {	
-	//Test 1
+	//Тест 1
 	int test1[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test1_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
 
-	//Test 2
+	//Тест 2
 	int test2[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int test2_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804};
 
-	//Sorting test arrays
+	//Сортуємо масиви
 	countingSort(test1);
 	countingSort(test2);
 
@@ -74,15 +74,15 @@ TEST(countingSort_test, performance)
 
 TEST(radixSort_test, performance)
 {
-	//Test 1
+	//Тест 1
 	int test1[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test1_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
 
-	//Test 2
+	//Тест 2
 	int test2[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int test2_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804 };
 
-	//Sorting test arrays
+	//Сортуємо масиви
 	radixSort(test1);
 	radixSort(test2);
 	EXPECT_EQ(1, comp(test1, test1_sorted));
