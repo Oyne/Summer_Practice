d2c959c0b04ac2d48075be4df82e29af11271e2d
Андрей Литвинов <andreoyy.litvinovv@outlook.com>
2022-07-10 15:49:23 +0300

    Increased the convenience of using the program and module

--- Sort_Test/test.cpp
+++ Sort_Test/test.cpp
@@ -2,13 +2,30 @@
 #include "..\Summer_Practice\sort_arr.c"
 
 //Функція яка перевіряє два масива на ідентичність
-int comp(int arr1[], int arr2[])
+int comp(int arr1[], int arr2[], unsigned int element_num)
 {
-	for (int i = 0; i < size; i++)
-	{
+	for (int i = 0; i < element_num; i++)
 		if (arr1[i] != arr2[i]) return 0;
-	}
 	return 1;
+}
+
+TEST(negative_found_test, performance)
+{
+	//Тест 1
+	int test1[] = { -132, -25, 345, 99, 123, 2004, 9, 0, 3, 10 };
+	size_t test1_size = sizeof test1 / sizeof * test1;
+
+	//Тест 2
+	int test2[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
+	size_t test2_size = sizeof test2 / sizeof * test2;
+
+	//Тест 3
+	int test3[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
+	size_t test3_size = sizeof test3 / sizeof * test3;
+
+	EXPECT_EQ(0, negative_found(test1, test1_size));
+	EXPECT_EQ(1, negative_found(test2, test2_size));
+	EXPECT_EQ(1, negative_found(test3, test3_size));
 }
 
 TEST(getMax_test, performance)
@@ -16,18 +33,21 @@ TEST(getMax_test, performance)
 	//Тест 1
 	int test1[] = { -132, -25, 345, 99, 123, 2004, 9, 0, 3, 10 };
 	int max1 = 2004;
+	size_t test1_size = sizeof test1 / sizeof * test1;
 
 	//Тест 2
 	int test2[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int max2 = 9573;
+	size_t test2_size = sizeof test2 / sizeof * test2;
 
 	//Тест 3
 	int test3[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int max3 = 9804;
+	size_t test3_size = sizeof test3 / sizeof * test3;
 
-	EXPECT_EQ(max1, getMax(test1));
-	EXPECT_EQ(max2, getMax(test2));
-	EXPECT_EQ(max3, getMax(test3));
+	EXPECT_EQ(max1, getMax(test1, test1_size));
+	EXPECT_EQ(max2, getMax(test2, test2_size));
+	EXPECT_EQ(max3, getMax(test3, test3_size));
 }
 
 TEST(mergeSort_test, performance)
@@ -35,42 +55,46 @@ TEST(mergeSort_test, performance)
 	//Тест 1
 	int test1[] = { -132, -25, 345, 99, 123, 2004, 9, 0, 3, 10 };
 	int test1_sorted[] = { -132, -25, 0, 3, 9, 10, 99, 123, 345, 2004 };
+	size_t test1_size = sizeof test1 / sizeof * test1;
 
 	//Тест 2
 	int test2[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test2_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
+	size_t test2_size = sizeof test2 / sizeof * test2;
 
 	//Тест 3
 	int test3[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int test3_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804 };
+	size_t test3_size = sizeof test3 / sizeof * test3;
 
 	//Сортуємо масиви
-	mergeSort(test1, 0, size - 1);
-	mergeSort(test2, 0, size - 1);
-	mergeSort(test3, 0, size - 1);
-	
-	EXPECT_EQ(1, comp(test1, test1_sorted));
-	EXPECT_EQ(1, comp(test2, test2_sorted));
-	EXPECT_EQ(1, comp(test3, test3_sorted));
+	mergeSort(test1, 0, test1_size - 1);
+	mergeSort(test2, 0, test2_size - 1);
+	mergeSort(test3, 0, test3_size - 1);
 
+	EXPECT_EQ(1, comp(test1, test1_sorted, test1_size));
+	EXPECT_EQ(1, comp(test2, test2_sorted, test2_size));
+	EXPECT_EQ(1, comp(test3, test3_sorted, test3_size));
 }
 
 TEST(countingSort_test, performance)
-{	
+{
 	//Тест 1
 	int test1[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test1_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
+	size_t test1_size = sizeof test1 / sizeof * test1;
 
 	//Тест 2
 	int test2[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
-	int test2_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804};
+	int test2_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804 };
+	size_t test2_size = sizeof test2 / sizeof * test2;
 
 	//Сортуємо масиви
-	countingSort(test1);
-	countingSort(test2);
+	countingSort(test1, test1_size);
+	countingSort(test2, test2_size);
 
-	EXPECT_EQ(1, comp(test1, test1_sorted));
-	EXPECT_EQ(1, comp(test2, test2_sorted));
+	EXPECT_EQ(1, comp(test1, test1_sorted, test1_size));
+	EXPECT_EQ(1, comp(test2, test2_sorted, test2_size));
 }
 
 TEST(radixSort_test, performance)
@@ -78,14 +102,17 @@ TEST(radixSort_test, performance)
 	//Тест 1
 	int test1[] = { 6073, 8277, 4612, 9573, 8443, 6061, 5690, 298, 8860, 2967 };
 	int test1_sorted[] = { 298, 2967, 4612 ,5690 ,6061 ,6073 ,8277, 8443 ,8860 ,9573 };
+	size_t test1_size = sizeof test1 / sizeof * test1;
 
 	//Тест 2
 	int test2[] = { 673, 3125, 215, 9804, 1023, 10, 9, 0, 1011, 54 };
 	int test2_sorted[] = { 0, 9, 10, 54, 215, 673, 1011, 1023, 3125, 9804 };
+	size_t test2_size = sizeof test2 / sizeof * test2;
 
 	//Сортуємо масиви
-	radixSort(test1);
-	radixSort(test2);
-	EXPECT_EQ(1, comp(test1, test1_sorted));
-	EXPECT_EQ(1, comp(test2, test2_sorted));
+	radixSort(test1, test1_size);
+	radixSort(test2, test2_size);
+
+	EXPECT_EQ(1, comp(test1, test1_sorted, test1_size));
+	EXPECT_EQ(1, comp(test2, test2_sorted, test2_size));
 }
--- Summer_Practice/main.c
+++ Summer_Practice/main.c
@@ -15,48 +15,48 @@
 int main()
 {
 	setlocale(LC_ALL, "ukr");
-	printf("Для сортування пiдрахунком : ручний ввiд >= 0 && < 100000\nДля сортування за розрядами : ручний ввiд >= 0\n");
+	printf("Для сортування пiдрахунком : ручний ввiд >= 0\nДля сортування за розрядами : ручний ввiд >= 0\n");
 	int user_auto, repeat, same, sort_type, arr[size], copy[size];
 	do
 	{
-	printf("\nБажаєте ввести масив самi чи автоматично?\nСамi - 1 | Автоматично - 0 : ");
-	scanf_s("%d", &user_auto);
-	if (user_auto) person_input(arr);
-	else random_input(arr);
-	printf("\nВаш масив:\n");
-	arr_print(arr);
-	do
-	{
-		arr_copy(arr, copy);
-		printf("\nЯкий алгоритм сортування ви бажаєте використати?\n");
-		printf("сортування злиттям - 1 | сортування пiдрахунком - 2 | сортування за розрядами - 3 : ");
-		scanf_s("%d", &sort_type);
-		switch (sort_type)
+		printf("\nБажаєте ввести масив самi чи автоматично?\nСамi - 1 | Автоматично - 0 : ");
+		scanf_s("%d", &user_auto);
+		if (user_auto) person_input(arr, size);
+		else random_input(arr, size);
+		printf("\nВаш масив:\n");
+		arr_print(arr, size);
+		do
 		{
-		case 1:
-			printf("\nМасив вiдсортований злиттям:\n");
-			mergeSort(copy, 0, size - 1);
-			arr_print(copy);
-			break;
-		case 2:
-			printf("\nМасив вiдсортований пiдрахунком:\n");
-			countingSort(copy);
-			arr_print(copy);
-			break;
-		case 3:
-			printf("\nМасив вiдсортований пiдрахунком за розрядами:\n");
-			radixSort(copy);
-			arr_print(copy);
-			break;
-		default:
-			printf("Ви ввели некоректне значення");
-			break;
-		}
-		printf("\nБажаєте вiдсортувати вхiдний масив знову?\nТак - 1 | Нi - 0 : ");
-		scanf_s("%d", &same);
-	} while (same);
-	printf("\nБажаєте продовжити?\nТак - 1 | Нi - 0 : ");
-	scanf_s("%d", &repeat);
+			arr_copy(arr, copy, size);
+			printf("\nЯкий алгоритм сортування ви бажаєте використати?\n");
+			printf("сортування злиттям - 1 | сортування пiдрахунком - 2 | сортування за розрядами - 3 : ");
+			scanf_s("%d", &sort_type);
+			switch (sort_type)
+			{
+			case 1:
+				mergeSort(copy, 0, size - 1);
+				printf("\nМасив вiдсортований злиттям:\n");
+				arr_print(copy, size);
+				break;
+			case 2:
+				countingSort(copy, size);
+				printf("\nМасив вiдсортований пiдрахунком:\n");
+				arr_print(copy, size);
+				break;
+			case 3:
+				radixSort(copy, size);
+				printf("\nМасив вiдсортований пiдрахунком за розрядами:\n");
+				arr_print(copy, size);
+				break;
+			default:
+				printf("Ви ввели некоректне значення");
+				break;
+			}
+			printf("\nБажаєте вiдсортувати вхiдний масив знову?\nТак - 1 | Нi - 0 : ");
+			scanf_s("%d", &same);
+		} while (same);
+		printf("\nБажаєте продовжити?\nТак - 1 | Нi - 0 : ");
+		scanf_s("%d", &repeat);
 	} while (repeat);
 	return 0;
 }
